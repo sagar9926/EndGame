4b449118d1aaf1f57415ca1a06974f692ef0fcdb
sagar9926 <sagarkumar9926@gmail.com>
2020-05-28 22:42:21 +0530

    Create custom_env.py

---
+++ gym_game/envs/custom_env.py
@@ -0,0 +1,28 @@
+import gym
+from gym import spaces
+import numpy as np
+from gym_game.envs.pygame_2d import PyGame2D
+from gym.spaces.box import Box
+
+class CustomEnv(gym.Env):
+    #metadata = {'render.modes' : ['human']}
+    def __init__(self):
+        self.pygame = PyGame2D()
+        self.action_space = Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+        self.observation_space = Box(low=0, high=255, shape=(28, 28, 3), dtype=np.int)
+
+    def reset(self):
+        del self.pygame
+        self.pygame = PyGame2D()
+        obs = self.pygame.observe()
+        return obs
+
+    def step(self, action):
+        self.pygame.action(action)
+        obs = self.pygame.observe()
+        reward = self.pygame.evaluate()
+        done = self.pygame.is_done()
+        return obs, reward, done, {}
+
+    def render(self, mode="human", close=False):
+        self.pygame.view()
